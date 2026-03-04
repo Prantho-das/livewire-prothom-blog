10cd00379860c41be76d7552586f5a4b1afa2392
Prantho-das <pranthokumardas786@gmail.com>
2026-03-05 02:06:25 +0600

    index  check

--- app/Livewire/Front/CategorySection.php
+++ app/Livewire/Front/CategorySection.php
@@ -33,8 +33,9 @@ class CategorySection extends Component
                     'translation' => fn ($q) => $q->select(['id', 'post_id', 'locale', 'title', 'excerpt']),
                     'categories' => fn ($q) => $q->select(['categories.id', 'categories.slug'])
                         ->with(['translation' => fn ($q) => $q->select(['id', 'category_id', 'locale', 'name'])]),
+                    'author' => fn ($q) => $q->select(['id', 'name']),
                 ])
-                ->select(['posts.id', 'posts.slug', 'posts.featured_image', 'posts.views_count', 'posts.published_at', 'posts.author_id'])
+                ->select(['posts.id', 'posts.slug', 'posts.featured_image', 'posts.is_featured', 'posts.is_breaking', 'posts.views_count', 'posts.published_at', 'posts.author_id'])
                 ->latest('published_at')
                 ->limit(4)
                 ->get()
@@ -42,8 +43,12 @@ class CategorySection extends Component
                     'id' => $p->id,
                     'slug' => $p->slug,
                     'featured_image' => $p->featured_image,
+                    'is_featured' => $p->is_featured ?? false,
+                    'is_breaking' => $p->is_breaking ?? false,
                     'title' => $p->translation?->title ?? 'Untitled',
+                    'excerpt' => $p->translation?->excerpt ?? '',
                     'published_at' => $p->published_at?->diffForHumans(),
+                    'author' => $p->author?->name ?? '',
                     'category_name' => $p->categories->first()?->translation?->name,
                     'category_slug' => $p->categories->first()?->slug,
                     'views_count' => $p->views_count,
